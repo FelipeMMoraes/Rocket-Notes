b8b97d7940d974126d4262ba64453ee5a8cb1fb2
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-16 16:28:57 -0300

    add: db

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 const uploadConfig = require ("./configs/upload")
 
+const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
 
 migrationsRun();
 
 const app = express();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
@@ -30,7 +32,7 @@ app.use(( error, request, response, next ) => {
     status: "error",
     message: "Internal server error",
   })
-})
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
